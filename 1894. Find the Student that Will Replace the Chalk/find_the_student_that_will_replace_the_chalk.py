85756b09fe5e935d031422ef06e8e241130c7957
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2024-09-02 10:42:24 +0800

    Solve 1894. Find the Student that Will Replace the Chalk

---
+++ 1894. Find the Student that Will Replace the Chalk/find_the_student_that_will_replace_the_chalk.py
@@ -0,0 +1,8 @@
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        k %= sum(chalk)
+        for i in range(len(chalk)):
+            if k < chalk[i]:
+                break
+            k -= chalk[i]
+        return i
