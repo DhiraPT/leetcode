ec893c457fde2b676ff7447a43df9d4589ab1b80
Dhiraputta Pathama Tengara <dhira.pt@gmail.com>
2024-09-28 14:17:27 +0800

    Solve 641. Design Circular Deque

---
+++ 641. Design Circular Deque/design_circular_deque.py
@@ -0,0 +1,66 @@
+class MyCircularDeque:
+
+    def __init__(self, k: int):
+        self.k = k
+        self.deque = [0] * k
+        self.front = 0
+        self.rear = -1
+        self.size = 0
+
+    def insertFront(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.front = (self.front - 1) if self.front != 0 else self.k - 1
+        self.deque[self.front] = value
+        self.size += 1
+        return True
+
+    def insertLast(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.rear = (self.rear + 1) if self.rear != self.k - 1 else 0
+        self.deque[self.rear] = value
+        self.size += 1
+        return True
+
+    def deleteFront(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.front = (self.front + 1) if self.front != self.k - 1 else 0
+        self.size -= 1
+        return True
+
+    def deleteLast(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.rear = (self.rear - 1) if self.rear != 0 else self.k - 1
+        self.size -= 1
+        return True
+
+    def getFront(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.deque[self.front]
+
+    def getRear(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.deque[self.rear]
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.k
+
+
+# Your MyCircularDeque object will be instantiated and called as such:
+# obj = MyCircularDeque(k)
+# param_1 = obj.insertFront(value)
+# param_2 = obj.insertLast(value)
+# param_3 = obj.deleteFront()
+# param_4 = obj.deleteLast()
+# param_5 = obj.getFront()
+# param_6 = obj.getRear()
+# param_7 = obj.isEmpty()
+# param_8 = obj.isFull()
